b739645ab76953a40d5a9402fead67b64e6e0be4
Raimon Esteve <raimonesteve@gmail.com>
2023-06-30 14:57:58 +0200

    Set origin in case model is in selection reference when create purchase from purchase requests
    #160335

--- operation.py
+++ operation.py
@@ -554,6 +554,11 @@ class PurchaseLine(metaclass=PoolMeta):
             origins += ['purchase.request']
         except KeyError:
             pass
+        try:
+            Pool().get('sale.sale')
+            origins += ['sale.sale']
+        except KeyError:
+            pass
         return origins
 
     @classmethod
@@ -586,9 +591,14 @@ class CreatePurchase(metaclass=PoolMeta):
 
     @classmethod
     def compute_purchase_line(cls, key, requests, purchase):
+        Line = Pool().get('purchase.line')
 
         line = super().compute_purchase_line(key, requests, purchase)
-        if requests:
-            request = requests[0]
-            line.origin = request.origin
+
+        origins = [k[0] for k in Line.get_origin()]
+
+        for origin in [request.origin for request in requests if request.origin]:
+            if origin.__name__ in origins:
+                line.origin = origin
+                break
         return line
